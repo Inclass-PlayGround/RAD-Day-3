8bce7976bda0466a674fd38d9e6275075d6a7f98
Dilan Fernando <dilanmadushanka6013@gmail.com>
2024-12-04 11:06:27 +0530

    add customer to arry list and print

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,54 @@
-import './App.css'
-import {ChangeEvent, useState} from "react";
+import './App.css';
+import { ChangeEvent, useState } from 'react';
+import {Customer} from "./Modles/Customer.ts";
 
 function App() {
+    const [customer, setCustomer] = useState<Customer[]>([]);
 
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [phone, setPhone] = useState<string>('');
 
-    // const  [customer ,setCustomer]
-    const [customer, setCustomer] = useState({
-        firstName : 0,
-        lastName : 0
-    });
-
-
-    function handleInputChange(event:ChangeEvent<HTMLInputElement>) {
-        setCustomer({
-            ...customer,
-        [event.target.name]: event.target.value
-
-        })
+    function addCustomer() {
+        const newCustomer = new Customer(name, email, phone);
+        setCustomer((prevCustomers) => [...prevCustomers, newCustomer]);
     }
 
-  return (
-    <div>
-        <input name={"firstName"} type="text" placeholder={"First Name"} onChange={handleInputChange}/>
-        <input name={"lastName"} type="text" placeholder={"Last Name"} onChange={handleInputChange}/>
+    return (
+        <div>
+            <input
+                name="name"
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+            />
+            <input
+                name="email"
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+            />
+            <input
+                name="phone"
+                type="text"
+                placeholder="Phone"
+                value={phone}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setPhone(e.target.value)}
+            />
 
-        <br/>
-        <h2>{"Hello "+customer.firstName +" "+customer.lastName}</h2>
+            <br />
+
+            <button onClick={addCustomer}>Add Customer</button>
 
 
-    </div>
-  )
+            {customer.map((cust) => (
+                <h2>{cust.name +" "+ cust.email +" "+ cust.phone}</h2>
+            ))}
+
+        </div>
+    );
 }
 
-
-export default App
+export default App;
